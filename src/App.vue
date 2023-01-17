89e4398fec274755b7317ebf02b56b10b6bacfa8
navid-nhd <nahardaninavid1993@gmail.com>
2023-01-17 13:56:45 +0330

    startup  and adding tailwind

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+
+
+<template>
+  <h1 class="bg-red-400">
+    Hello world!!
+  </h1>
+</template>
+
+<script >
+
+
+
+
+export default{
+  components: {
+
+  },
+  data (){
+    return{
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
